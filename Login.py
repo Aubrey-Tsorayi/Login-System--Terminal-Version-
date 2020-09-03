d978448b57b3865f842f43cab07d64bb875b570b
Tutsirayi <tutsirayit.tt@gmail.com>
2020-09-03 13:36:04 +0200

    Fixed order, linked login fuction

--- Login.py
+++ Login.py
@@ -10,22 +10,16 @@ while run:
     print("////////////////////////////")
     print("        Welcome             ")
     print("////////////////////////////")
-    choice = eval(input('1. Log in 2. Create account 3. Quit : '))
+    choice = eval(input('1. Create account 2. Login 3. Quit : '))
     if choice == 1:
-        name = input('Enter user name: ')
-        if name == username:
-            pin = input('Enter password: ')
-            if pin == password:
-                print("Access Granted")
-            else:
-                print("Access Deined")
-        else:
-            print("Username name not found")
-    elif choice == 2:
         username = input('Create username: ')
         password = input('Create password: ')
         database.add_user(username,password)
         print('Account created')
+    elif choice == 2:
+        database.login()
+        print("Login succees")
+        break
     elif choice == 3:
         run = False
 
--- database.py
+++ database.py
@@ -18,3 +18,30 @@ def add_user(username,password):
 	c.execute("INSERT INTO info VALUES (?,?)",info)
 	con.commit()
 	con.close()
+
+#login user
+def login():
+	while True:
+		username = input("Enter username: ")
+		password = input("Enter password: ")
+		with sqlite3.connect("users.db") as db:
+				c = db.cursor()
+		finduser = ("SELECT * FROM info WHERE username = ? AND password = ?")
+		c.execute(finduser,[(username),(password)])
+		results = c.fetchall()
+
+		if results:
+			for i in results:
+				print("Welcome " + i[0].upper())
+			return("exit")
+			break
+
+		else:
+			print("Username and password incorrect")
+			again = input("Do you with to try again? (y/n): ")
+			if again.lower() == "n":
+				print("Goodbye")
+				time.sleep(1)
+			break
+
+print("Done")
